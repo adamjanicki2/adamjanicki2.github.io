22526ce39f54c6294f8b41d6941984a0c444f219
Adam Janicki <adamjanicki2@gmail.com>
2024-04-29 16:39:18 -0400

    scroll to window hash

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,16 +4,20 @@ import Home from "src/components/Home";
 import Projects from "src/components/Projects";
 import Contact from "src/components/Contact";
 import Footer from "src/components/Footer";
+import { useScrollToHash } from "@adamjanicki/core-ui";
 
-const App = () => (
-  <>
-    <Nav />
-    <Home />
-    <About />
-    <Projects />
-    <Contact />
-    <Footer />
-  </>
-);
+const App = () => {
+  useScrollToHash();
+  return (
+    <>
+      <Nav />
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
 
 export default App;

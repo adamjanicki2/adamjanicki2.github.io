b1df47d10e0af7bb501d9517d37693f085b59158
Adam Janicki <adamjanicki2@gmail.com>
2024-08-18 11:54:45 -0700

    adjust select z index

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,13 +7,12 @@ const Footer = () => (
         slug="The_Silence_of_the_Lambs"
         text="I'm having an old friend for dinner."
       />
-      <p className="ma0 tr" style={{ fontSize: "10px" }}>
+      <p className="ma0 tr desktop" style={{ fontSize: "10px" }}>
         Did someone say... hidden movie quotes?
         <br />
         Hint: use ⌘A or tab!
       </p>
     </div>
-
     <p className="fw5 f5">Est. 2023 Built from scratch by Adam</p>
     <div className="flex justify-end w-100 pa2">
       <Secret

02aa1439c70ce376e0dc2d8924736bd6bcbf3c5a
Adam Janicki <adamjanicki2@gmail.com>
2025-02-20 21:33:15 -0800

    center text

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => (
     className="marquee flex flex-column items-center justify-between white"
   >
     <div />
-    <h1 className="home-welcome">Hi, I'm Adam.</h1>
+    <h1 className="home-welcome tc">Hi, I'm Adam.</h1>
     <div className="flex w-100 justify-between pa2">
       <Secret
         slug="Star_Wars:_Episode_III_-_Revenge_of_the_Sith"

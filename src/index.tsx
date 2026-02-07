9972ff535f67e3a20e3046eb7d5093914c0074db
Adam Janicki <adamjanicki2@gmail.com>
2026-02-07 13:02:13 -0800

    final tweaks

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import "@adamjanicki/ui/style.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "src/components/App";
-import "src/css/media.css";
 import "src/css/style.css";
+// media comes last
+import "src/css/media.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
